7bf8a333cf7c9990adfa063e59fe73c53a1f23e9
GitStudent2022 <torelgame@mail.ru>
2022-09-24 16:19:10 +0300

    Добавьте файлы проекта.

---
+++ OopAdvansedProject/MyString.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OopAdvansedProject
+{
+    class MyString
+    {
+        public string Str { set; get; }
+
+        public string ToUpper()
+        {
+            return Str.ToUpper();
+        }
+
+        public int CountChar(char ch)
+        {
+            int count = 0;
+            foreach (char c in Str)
+                if (c == ch)
+                    count++;
+            return count;
+        }
+    }
+
+    public static class StringExtencion
+    {
+        public static int CountChar(this string str, char ch)
+        {
+            int count = 0;
+            foreach (char c in str)
+                if (c == ch)
+                    count++;
+            return count;
+        }
+    }
+
+    partial class User
+    {
+        public void Print()
+        {
+            Console.WriteLine($"{Name} {Id}");
+        }
+    }
+}
---
+++ OopAdvansedProject/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace OopAdvansedProject
+{
+    partial class User
+    {
+        public int Id { get; set; }
+    }
+
+    internal class Program
+    {
+        public static ref int Method(ref int[] array)
+        {
+            return ref array[0];
+        }
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello, World!");
+            //int n = 100;
+            //int[] a = new int[3] { 1, 2, 3 };
+            //int s = Method(ref a);
+
+            ////MyString myString = new() { Str = "Hello world" };
+
+            //string myString = "Hello world";
+
+            //Console.WriteLine(myString.CountChar('l'));
+
+            //User user = new User() { Id = 123, Name = "Bob" };
+            //user.Print();
+
+            var userHobby = new { Id = 123, Name = "Tom", Company = "Yandex" };
+            Console.WriteLine(userHobby.GetType());
+
+            var tuple = (123, "qwerty");
+            Console.WriteLine(tuple.GetType());
+
+            (int, string) t = (23, "aaaa");
+
+            (int, int, string, float) tt = (id:123, count:456, passw:"qwerty", price:123.456f);
+        }
+    }
+}
